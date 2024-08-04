2879e2c0757549cf2cf2b15c62a9def8c5efbfe7
Tegar Levta Anggana <tegarla123@gmail.com>
2024-08-04 07:29:21 +0700

    first commit

---
+++ renderer/pages/home.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import Head from "next/head";
+import Link from "next/link";
+import Image from "next/image";
+
+export default function HomePage() {
+  return (
+    <React.Fragment>
+      <Head>
+        <title>Home - Nextron (with-tailwindcss)</title>
+      </Head>
+      <button className="btn btn-primary">Primary</button>
+      <div className="grid grid-col-1 text-2xl w-full text-center">
+        <div>
+          <Image
+            className="ml-auto mr-auto"
+            src="/images/logo.png"
+            alt="Logo image"
+            width={256}
+            height={256}
+          />
+        </div>
+        <span>⚡ Electron ⚡</span>
+        <span>+</span>
+        <span>Next.js</span>
+        <span>+</span>
+        <span>tailwindcss</span>
+        <span>=</span>
+        <span>💕 </span>
+        <img
+          src="https://www.w3schools.com/images/lamp.jpg"
+          alt="Lamp"
+          width="32"
+          height="32"
+        />
+      </div>
+      <div className="mt-1 w-full flex-wrap flex justify-center">
+        <Link href="/next">Go to next page</Link>
+      </div>
+    </React.Fragment>
+  );
+}
